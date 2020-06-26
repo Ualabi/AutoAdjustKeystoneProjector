a90f3e839142521ee89bf525fac22e6d63e00f7f
Joseandres Hinojoza <57422356+Ualabi@users.noreply.github.com>
2020-06-26 07:28:23 -0500

    Update A4988.cpp

--- A4988.cpp
+++ A4988.cpp
@@ -1,9 +1,5 @@
 #include "A4988.h"
 
-/*
- * Microstepping resolution truth table (Page 6 of A4988 pdf)
- * 0bMS3,MS2,MS1 for 1,2,4,8,16 microsteps
- */
 const uint8_t A4988::MS_TABLE[] = {0b000, 0b001, 0b010, 0b011, 0b111};
 
 A4988::A4988(short steps, short dir_pin, short step_pin)
@@ -24,30 +20,20 @@ A4988::A4988(short steps, short dir_pin, short step_pin, short enable_pin, short
 ms1_pin(ms1_pin), ms2_pin(ms2_pin), ms3_pin(ms3_pin)
 {}
 
-//Funcion agregada por Moisès Vivar
-A4988::A4988(short steps, short dir_pin, short step_pin, short stop_down, short stop_up/*, short ms1_pin, short ms2_pin, short ms3_pin*/)
-:BasicStepperDriver(steps, dir_pin, step_pin, stop_down, stop_up)/*,
-ms1_pin(ms1_pin), ms2_pin(ms2_pin), ms3_pin(ms3_pin)*/
+A4988::A4988(short steps, short dir_pin, short step_pin, short stop_down, short stop_up) //Funcion agregada por JH
+:BasicStepperDriver(steps, dir_pin, step_pin, stop_down, stop_up)
 {}
-//////////////////////////////////
 
 void A4988::begin(short rpm, short microsteps){
     BasicStepperDriver::begin(rpm, microsteps);
 
-    if (!IS_CONNECTED(ms1_pin) || !IS_CONNECTED(ms2_pin) || !IS_CONNECTED(ms3_pin)){
-        return;
+    if (IS_CONNECTED(ms1_pin) && IS_CONNECTED(ms2_pin) && IS_CONNECTED(ms3_pin)){
+        pinMode(ms1_pin, OUTPUT);
+        pinMode(ms2_pin, OUTPUT);
+        pinMode(ms3_pin, OUTPUT);
     }
-
-    pinMode(ms1_pin, OUTPUT);
-    pinMode(ms2_pin, OUTPUT);
-    pinMode(ms3_pin, OUTPUT);
 }
 
-/*
- * Set microstepping mode (1:divisor)
- * Allowed ranges for A4988 are 1:1 to 1:16
- * If the control pins are not connected, we recalculate the timing only
- */
 short A4988::setMicrostep(short microsteps){
     BasicStepperDriver::setMicrostep(microsteps);
 
